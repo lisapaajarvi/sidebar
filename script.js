c159afde13fa88f269debc930a8cec4678f711da
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2020-11-17 11:39:45 +0100

    added close sidebar function

--- script.js
+++ script.js
@@ -5,12 +5,31 @@ function main() {
 }
 
 function addEventListeners() {
-    const sidebarButton = document.getElementById("open-sidebar-button");
-    sidebarButton.onclick = openSidebar;
+    const openSidebarButton = document.getElementById("open-sidebar-button");
+    openSidebarButton.onclick = openSidebar;
+
+    const closeSidebarIcon = document.getElementById("close-sidebar-icon")
+    closeSidebarIcon.onclick = closeSidebar;
 }
 
 function openSidebar() {
     const sidebar = document.getElementById("sidebar-div")
-    console.log(sidebar);
     sidebar.style.width = "20rem";
+
+    const menuList = document.getElementById("menu-list")
+    menuList.style.display= "flex";
+
+    const settings = document.getElementById("settings-div")
+    settings.style.display = "flex";
+}
+
+function closeSidebar() {
+    const sidebar = document.getElementById("sidebar-div")
+    sidebar.style.width = "0";
+
+    const menuList = document.getElementById("menu-list")
+    menuList.style.display= "none";
+
+    const settings = document.getElementById("settings-div")
+    settings.style.display = "none";
 }
