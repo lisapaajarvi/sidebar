7b45a4f35b686952a115f6db99cd09df124692df
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2020-11-17 14:51:17 +0100

    changed sidebar function

--- script.js
+++ script.js
@@ -10,26 +10,17 @@ function addEventListeners() {
 
     const closeSidebarIcon = document.getElementById("close-sidebar-icon")
     closeSidebarIcon.onclick = closeSidebar;
+
+    /*const menuSearch = document.getElementById("search")
+    menuSearch.onmouseover = highlightMenuItem;*/
 }
 
 function openSidebar() {
     const sidebar = document.getElementById("sidebar-div")
     sidebar.style.width = "20rem";
-
-    const menuList = document.getElementById("menu-list")
-    menuList.style.display= "flex";
-
-    const settings = document.getElementById("settings-div")
-    settings.style.display = "flex";
 }
 
 function closeSidebar() {
     const sidebar = document.getElementById("sidebar-div")
-    sidebar.style.width = "0";
-
-    const menuList = document.getElementById("menu-list")
-    menuList.style.display= "none";
-
-    const settings = document.getElementById("settings-div")
-    settings.style.display = "none";
-}
+    sidebar.style.width = "0"; 
+} 
